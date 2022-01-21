8ed0ea4bd0d255e2563f18f773586c4fb7749b72
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-01-21 16:35:50 +0200

    Initial Commit

---
+++ Location Router/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location Router
+//
+//  Created by Oksana Poliakova on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
