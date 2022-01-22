38a729f2596a6e2cf047359830a8be5bf71ebbf3
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-01-22 17:36:52 +0200

    Created an interface in the Main.stroryboard

--- Location Router/Controllers/ViewController.swift
+++ Location Router/Controllers/ViewController.swift
@@ -10,23 +10,33 @@ import CoreLocation
 import MapKit
 
 class ViewController: UIViewController {
-    
     // MARK: - Properties
+    
     private let locationManager = CLLocationManager()
     private lazy var latitude = locationManager.location?.coordinate.latitude
     private lazy var longitude = locationManager.location?.coordinate.latitude
     private var currentPlace: CLPlacemark?
+    private let completer = MKLocalSearchCompleter()
     
+    @IBOutlet weak var nameOfPlace: UITextField!
+    @IBOutlet weak var stopAddress: UITextField!
+    @IBOutlet weak var extraStopAddress: UITextField!
+    
+    // MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        nameOfPlace.addTarget(self, action: #selector(changeNameOfPlace(_:)), for: .allEditingEvents)
+        stopAddress.addTarget(self, action: #selector(changeStopAddress(_:)), for: .allEditingEvents)
+        extraStopAddress.addTarget(self, action: #selector(changeExtraStopAddress(_:)), for: .allEditingEvents)
         setupLocation()
+        completer.delegate = self
     }
-    
+
     // MARK: - Setup location
+    
     private func setupLocation() {
         guard CLLocationManager.locationServicesEnabled() else { return }
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
         locationManager.requestLocation()
         locationManager.requestWhenInUseAuthorization()
@@ -34,6 +44,7 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Location authorization status
+    
     private func locationAuthorizationStatus() -> CLAuthorizationStatus {
         var locationAuthorizationStatus : CLAuthorizationStatus
         if #available(iOS 14.0, *) {
@@ -43,22 +54,70 @@ class ViewController: UIViewController {
         }
         return locationAuthorizationStatus
     }
-
+    
+    
+    @objc private func changeNameOfPlace(_ sender: UITextField) {
+        textFieldDidChange(sender)
+    }
+    
+    @objc private func changeStopAddress(_ sender: UITextField) {
+    }
+    
+    @objc private func changeExtraStopAddress(_ sender: UITextField) {
+    }
+    
+    private func textFieldDidChange(_ field: UITextField) {
+        if field == nameOfPlace && currentPlace != nil {
+          currentPlace = nil
+          field.text = ""
+        }
+        
+        guard let query = field.text else {
+          if completer.isSearching {
+            completer.cancel()
+          }
+          return
+        }
+        
+        completer.queryFragment = query
+    }
+    
+//    private func showSuggestion(_ suggestion: String) {
+//      suggestionLabel.text = suggestion
+//      suggestionContainerTopConstraint.constant = -4 // to hide the top corners
+//
+//      UIView.animate(withDuration: defaultAnimationDuration) {
+//        self.view.layoutIfNeeded()
+//      }
+//    }
 }
 
 // MARK: - CLLocationManagerDelegate
+
 extension ViewController: CLLocationManagerDelegate {
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.first != nil {
             print("Location")
         }
         
-        CLGeocoder().reverseGeocodeLocation(firstLocation) { places, _ in
-//            guard let firstPlace = places.first
+        guard let location = locations.first else { return }
+        
+        /// Returns an array of placemarks in its completion handler
+        CLGeocoder().reverseGeocodeLocation(location) { [weak self] places, _ in
+            guard
+                let self = self,
+                let firstPlace = places?.first,
+                self.nameOfPlace.text == nil
+            else {
+                return
+            }
+            
+            /// Store location and update
+            self.currentPlace = firstPlace
+            self.nameOfPlace.text = firstPlace.name
+        }
     }
     
-    /// Requesting Location Authorization
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedAlways:
@@ -81,3 +140,21 @@ extension ViewController: CLLocationManagerDelegate {
     }
 }
 
+// MARK: - MKLocalSearchCompleterDelegate
+
+extension ViewController: MKLocalSearchCompleterDelegate {
+  func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+    guard let firstResult = completer.results.first else {
+      return
+    }
+    
+    showSuggestion(firstResult.title)
+  }
+
+  func completer(
+    _ completer: MKLocalSearchCompleter,
+    didFailWithError error: Error
+  ) {
+    print("Error suggesting a location: \(error.localizedDescription)")
+  }
+}
